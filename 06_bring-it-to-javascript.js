40acd42008f23284d38e3cbe517c3fe3babeb948
devmontri-gitlhub <dev.montri@gmail.com>
2026-03-10 16:42:59 +0700

    v2_add something

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -3,7 +3,7 @@ id_product :"p000001",
 name_product : "SSD Samsung",
 detail_product :"SSD Storage 1 TB",
 count_product :"50",
-price_product :"1250"
+price_product :"1,250"
 }
 
 const c_id_product = product_1.id_product ;
@@ -18,5 +18,4 @@ console.log(
     'รายละเอียดสินค้า :', c_detail_product , 
     'จำนวนสินค้า :', c_count_product ,
     'ราคาสินค้า :', c_price_product ,
-
 ) ;
