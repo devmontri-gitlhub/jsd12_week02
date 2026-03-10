65a6edbc7026a1fe51bc330d9b68bf364b4698ee
devmontri-gitlhub <dev.montri@gmail.com>
2026-03-10 16:12:59 +0700

    add week_2

---
+++ 06_bring-it-to-javascript.js
@@ -0,0 +1,22 @@
+let product_1 = {
+id_product :"p000001",
+name_product : "SSD Samsung",
+detail_product :"SSD Storage 1 TB",
+count_product :"50",
+price_product :"1250"
+}
+
+const c_id_product = product_1.id_product ;
+const c_name_product = product_1.name_product ;
+const c_detail_product = product_1.detail_product ;
+const c_count_product = product_1.count_product ;
+const c_price_product = product_1.price_product ;
+
+console.log(
+    'รหัสสินค้า :', c_id_product, 
+    'ชื่อสินค้า :', c_name_product, 
+    'รายละเอียดสินค้า :', c_detail_product , 
+    'จำนวนสินค้า :', c_count_product ,
+    'ราคาสินค้า :', c_price_product ,
+
+) ;
